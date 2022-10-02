bc3069906d2c5be5228a4a4498aae50b704fd451
Alterys <krgtskrgts005@gmail.com>
2022-10-02 22:05:32 +0300

    First commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.calculator;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main);
+        EditText number1 = findViewById(R.id.number_one);
+        EditText number2 = findViewById(R.id.number_two);
+        Button button = findViewById(R.id.addition);
+        TextView result = findViewById(R.id.result);
+        button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View view) {
+                if (!number1.getText().toString().equals("") && !number2.getText().toString().equals("")) {
+                    double r;
+                    r = Double.parseDouble(number1.getText().toString()) +
+                            Double.parseDouble(number2.getText().toString());
+                    result.setText(Double.toString(r));
+                }
+            }
+        });
+
+    }
+}
