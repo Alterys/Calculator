2816a8890621d3a7a7829e1237da91a84359c134
Alterys <krgtskrgts005@gmail.com>
2022-10-04 23:32:58 +0300

    First commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,36 +2,126 @@ package com.example.calculator;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.os.PersistableBundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.calculator.databinding.ActivityMainBinding;
+
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+
+    private String buffer = "0";
+
+    ActivityMainBinding binding = null;
+
+    @SuppressLint("SetTextI18n")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        EditText number1 = findViewById(R.id.number_one);
-        EditText number2 = findViewById(R.id.number_two);
-        Button button = findViewById(R.id.addition);
-        TextView result = findViewById(R.id.result);
-        button.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View view) {
-                if (!number1.getText().toString().equals("") && !number2.getText().toString().equals("")) {
-                    double r;
-                    r = Double.parseDouble(number1.getText().toString()) +
-                            Double.parseDouble(number2.getText().toString());
-                    result.setText(Double.toString(r));
-                }
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.numberZero.setOnClickListener(view -> {
+            if (buffer.length() == 1 && buffer.charAt(0) == '0'){
+                return;
+            } else {
+                buffer += "0";
             }
+            binding.textView.setText(buffer);
         });
+
+        binding.numberOne.setOnClickListener(view -> {
+            if (buffer.length() == 1 && buffer.charAt(0) == '0'){
+                buffer = "1";
+            } else {
+                buffer += "1";
+            }
+            binding.textView.setText(buffer);
+        });
+        binding.numberTwo.setOnClickListener(view -> {
+            if (buffer.length() == 1 && buffer.charAt(0) == '0'){
+                buffer = "2";
+            } else {
+                buffer += "2";
+            }
+            binding.textView.setText(buffer);
+        });
+        binding.numberThree.setOnClickListener(view -> {
+            if (buffer.length() == 1 && buffer.charAt(0) == '0'){
+                buffer = "3";
+            } else {
+                buffer += "3";
+            }
+            binding.textView.setText(buffer);
+        });
+        binding.numberFour.setOnClickListener(view -> {
+            if (buffer.length() == 1 && buffer.charAt(0) == '0'){
+                buffer = "4";
+            } else {
+                buffer += "4";
+            }
+            binding.textView.setText(buffer);
+        });
+        binding.numberFive.setOnClickListener(view -> {
+            if (buffer.length() == 1 && buffer.charAt(0) == '0'){
+                buffer = "5";
+            } else {
+                buffer += "5";
+            }
+            binding.textView.setText(buffer);
+        });
+        binding.numberSix.setOnClickListener(view -> {
+            if (buffer.length() == 1 && buffer.charAt(0) == '0'){
+                buffer = "6";
+            } else {
+                buffer += "6";
+            }
+            binding.textView.setText(buffer);
+        });
+        binding.numberSeven.setOnClickListener(view -> {
+            if (buffer.length() == 1 && buffer.charAt(0) == '0'){
+                buffer = "7";
+            } else {
+                buffer += "7";
+            }
+            binding.textView.setText(buffer);
+        });
+        binding.numberEight.setOnClickListener(view -> {
+            if (buffer.length() == 1 && buffer.charAt(0) == '0'){
+                buffer = "8";
+            } else {
+                buffer += "8";
+            }
+            binding.textView.setText(buffer);
+        });
+        binding.numberNine.setOnClickListener(view -> {
+            if (buffer.length() == 1 && buffer.charAt(0) == '0'){
+                buffer = "9";
+            } else {
+                buffer += "9";
+            }
+            binding.textView.setText(buffer);
+        });
+        binding.clear.setOnClickListener(view -> {
+            buffer = "0";
+            binding.textView.setText(buffer);
+        });
+        binding.plus.setOnClickListener(view -> {
+            if (buffer.length() == 1 && buffer.charAt(0) == '0'){
+                buffer = "0";
+            }
+            if (buffer.charAt(0) == '+') {
+                buffer = "";
+            } else {
+                buffer = "+";
+            }
+
+            binding.textView.setText(buffer);
+        });
+
+
 
     }
 }
+
+
